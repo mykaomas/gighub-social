65465b860ada545478e959c2246bd732bc14c138
Greg Yerkes <gyerkes@rajant.com>
2024-01-06 19:12:06 -0500

    Working initial database

--- routes/api/bandRoutes.js
+++ routes/api/bandRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const band = require('../../models/Bands');
 
-// Updates book based on its book_id
-router.put('/:band_id', async (req, res) => {
+router.get('/', async (req, res) => {
+  const bandData = await band.findAll();
+
+  return res.json(bandData);
+})
+
+router.post('/', async (req, res) => {
+  const bookData = await band.create(req.body);
+
+  return res.json(bandData);
+});
+
+// Updates book based on its band_id
+router.put('/', async (req, res) => {
   //Calls the update method on the Book model
   const updatedBand = await band.update(
     {
@@ -12,7 +24,7 @@ router.put('/:band_id', async (req, res) => {
       genre: req.body.genre,
     },
     {
-      // Gets a book based on the book_id given in the request parameters
+      // Gets a band based on the band_id given in the request parameters
       where: {
         band_id: req.params.band_id,
       },
