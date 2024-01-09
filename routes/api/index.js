5c48005a4208bb6d05f94e3c6a888bff7abc647a
Greg Yerkes <gyerkes@rajant.com>
2024-01-08 20:00:00 -0500

    added user database

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const bands = require('./bandRoutes');
+const users = require('./bandUsers');
 
 
 router.use('/bands', bands);
+router.use('./users', users)
 
 module.exports = router;
