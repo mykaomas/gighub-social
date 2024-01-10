cd12b2803ece31e09e76887c622090742111f42e
Greg Yerkes <gyerkes@rajant.com>
2024-01-09 19:57:12 -0500

    started session code

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -7,6 +8,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const sess = {
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: false,
+};
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
